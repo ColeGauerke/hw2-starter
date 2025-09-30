d49183c90376cce340faccbb9ea92847e94c5721
Cole Gauerke <cgauer1@lsu.edu>
2025-09-30 08:14:41 -0500

    HW 2 With Tests

--- src/basics.ml
+++ src/basics.ml
@@ -6,28 +6,46 @@ let abs x =
 (* Part 1: Non-Recursive Functions *)
 (***********************************)
 
-let rev_tup (tup : 'a * 'b) = failwith "unimplemented"
-let rev_triple (tup : 'a * 'b * 'c) = failwith "unimplemented"
+let rev_tup (tup : 'a * 'b) = 
+  let (a,b) = tup in (b,a)
 
-let is_odd x = failwith "unimplemented"
+let rev_triple (tup : 'a * 'b * 'c) =
+  let (a,b,c) = tup in (c,b,a)
 
-let is_older (date1: int * int * int) (date2: int * int * int) = failwith "unimplemented"
+let is_odd x = 
+  if x mod 2 <> 0 then true else false
 
-let to_us_format (date1: int * int * int) = failwith "unimplemented"
+let is_older (date1: int * int * int) (date2: int * int * int) =
+  date1 < date2
+
+let to_us_format (date1: int * int * int) = 
+  let (a,b,c) = date1 in (b,c,a)
+
 (*******************************)
 (* Part 2: Recursive Functions *)
-(*******************************)
+(*******************************) 
 
-let rec pow x p = failwith "unimplemented"
+let rec pow x p = 
+  if p = 0 then 1 else x * pow x (p-1)
 
-let rec fac n = failwith "unimplemented"
+let rec fac n = 
+  if n = 0 then 1
+  else if n = 1 then 1 else  n * fac(n-1)
 
 (*****************)
 (* Part 3: Lists *)
 (*****************)
 
-let rec get_nth ((idx:int), (lst: 'a list)) = failwith "unimplemented"
+let rec get_nth ((idx:int), (lst: 'a list)) = 
+  List.nth lst idx
 
-let larger lst1 lst2 = failwith "unimplemented"
+let larger lst1 lst2 = 
+  let len1 = List.length lst1 in
+  let len2 = List.length lst2 in
+  if len1 = len2 then []
+  else if len1 > len2 then lst1
+  else lst2
 
-let sum lst1 lst2 = failwith "unimplemented"
+let sum lst1 lst2 =  
+  let combined = lst1 @ lst2 in
+  List.fold_left (+) 0 combined
--- test/student/student.ml
+++ test/student/student.ml
@@ -2,11 +2,35 @@ open OUnit2
 open Basics
 
 let test_sanity _ =
-    assert_equal 1 1 ~msg:"Custom error message"
+  assert_equal 1 1 ~msg:"Student Tests"
+    
+let test_rev_tup _ = 
+  assert_equal (10,11) (rev_tup (11, 10)) ~msg:"rev_tup (5)";
+  assert_equal ('a','b') (rev_tup ('b', 'a')) ~msg:"rev_tup (6)";
+  assert_equal (1.0,5.0) (rev_tup (5.0, 1.0)) ~msg:"rev_tup (7)"
+
+let test_rev_triple _ = 
+  assert_equal (3.0, 4.0, 5.0) (rev_triple (5.0,4.0,3.0)) ~msg:"rev_triple (5)";
+  assert_equal ('a','b','c') (rev_triple ('c','b','a')) ~msg:"rev_triple (6)";
+  assert_equal (100,101,102) (rev_triple (102,101,100)) ~msg:"rev_triple (7)"
+
+let test_is_odd _ = 
+  assert_equal true (is_odd 9) ~msg:"is_odd (5)";
+  assert_equal true (is_odd 11) ~msg:"is_odd (6)";
+  assert_equal true (is_odd 13) ~msg:"is_odd (7)"
+
+let test_pow _ = 
+  assert_equal 8 (pow 2 3) ~msg:"pow (5)";
+  assert_equal 1 (pow 1 8) ~msg:"pow (6)";
+  assert_equal 16 (pow 4 2) ~msg:"pow (7)"
 
 let suite =
   "student" >::: [
-    "sanity" >:: test_sanity
+    "sanity" >:: test_sanity;
+    "rev_tup" >:: test_rev_tup;
+    "rev_trip" >:: test_rev_triple;
+    "is_odd" >:: test_is_odd;
+    "powow" >:: test_pow
   ]
 
 let _ = run_test_tt_main suite
